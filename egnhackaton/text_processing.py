e00955c5f55d8410bc91fcfa8a28e8cc2a42e60c
Étienne Bourbeau <etienne.bourbeau@alexandra.dk>
2023-08-31 13:38:44 +0200

    added retrieval function from embeddings

--- egnhackaton/text_processing.py
+++ egnhackaton/text_processing.py
@@ -10,18 +10,18 @@ openai.api_key = "ff9de7d753b5443b9846bfb3de8e6edb"
 
 EMBEDDING_DTYPE = np.float32
 
-def generate_embedding(text:str) -> np.ndarray:
+def get_embedding_from_input(input:str) -> np.ndarray:
 	"""
 	function to get embedding from openai
 
 	Args:
-		text (str): string to get embedding for
+		text (str): string to get embedding for (must be cleaned of punctuation)
 
 	Returns:
 		np.ndarray: numpy array of embedding
 	"""
 
-	embedding = openai.Embedding.create(input=text, engine='text-embedding-ada-002')['data'][0]['embedding']
+	embedding = openai.Embedding.create(input=input, engine='text-embedding-ada-002')['data'][0]['embedding']
 	return np.array(embedding, dtype=EMBEDDING_DTYPE)
 
 
@@ -34,17 +34,45 @@ def process_batch_data(input_chunks):
 	"""
 	embeddings = []
 	for chunk in input_chunks:
-		embeddings.append(generate_embedding(chunk['txt']))
+		print(chunk)
+		embeddings.append(get_embedding_from_input(chunk['txt']))
 	return embeddings
 
 
+def load_embeddings():
+	embeddings = pickle.load(open('./embeddings.pckl','rb'))
+
+
+def retrieve_from_indices(indices:list):
+	"""Given a list of row numbers, retrieve the relevant
+	text snippets from the original tex
+	"""
+
+	original_text = json.load(open('../../hackathon_data/queens_speeches/embeddings_data/datafile-18318495166876.json'))
+
+	max_l = len(original_text[0])
+	selected_statements = []
+
+	for i in indices:
+		selected_statements.append(original_text[0][i])
+	return selected_statements
+
+
 if __name__=='__main__':
+	import os
+	import argparse
+	import pickle
 
+	parser = argparse.ArgumentParser('produce embeddings')
+	parser.add_argument('-i','--input-json', help='name of json file', default='./example.json')
+	parser.add_argument('-o', '--output', help='output pckl file', default='embeddings.pckl')
+	args = parser.parse_args()
 
-	input_data = json.load(open('./example.json'))
-	print(input_data['txt'])
-
-	embeddings = process_batch_data([input_data])
-
-	print(embeddings)
-		
+	if os.path.isfile(args.output):
+		embeddings = pickle.load(open(args.output, 'rb'))
+	else:
+		input_data = json.load(open(args.input_json))
+		embeddings = process_batch_data(input_data[0])
+		pickle.dump(embeddings, open(args.output,'wb'))
+	for element in retrieve_from_indices([0,1,5]):
+		print(element)
